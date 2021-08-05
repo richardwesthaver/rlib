2863a5da924729e79806838825d9b2aed8647942
ellis <ellis@rwest.io>
2021-08-04 23:12:45 -0500

    pre-commit before jumbo migration

--- kala/src/ctl.rs
+++ kala/src/ctl.rs
@@ -1,7 +1,8 @@
+use std::collections::HashMap;
 use std::{fs::File, path::PathBuf, str::FromStr};
 
 pub use flate::pack;
-use logger::log::{error, trace};
+use logger::log::{error, info, trace};
 use net::client::{
   ipapi::my_ip,
   nws::{get_forecast, get_point},
@@ -9,19 +10,43 @@ use net::client::{
 };
 use weather::Point;
 
-pub fn usb_devices() {
-  println!("---------------------------------------");
-  println!("--++-- USB DEVICES --++--");
+use crate::Result;
+
+pub fn usb_devices(usbmap: HashMap<(u16, u16), String>) -> Result<()> {
+  // basic local info, useful for debugging
+  trace!("has capability? {}", rusb::has_capability());
+  trace!("has hotplug? {}", rusb::has_hotplug());
+  trace!("has HID access? {}", rusb::has_hid_access());
+  trace!(
+    "supports detach kernel driver? {}",
+    rusb::supports_detach_kernel_driver()
+  );
+
   for device in rusb::devices().unwrap().iter() {
     let device_desc = device.device_descriptor().unwrap();
-    println!(
+    info!(
       "Bus {:03} Device {:03} ID {:04x}:{:04x}",
       device.bus_number(),
       device.address(),
       device_desc.vendor_id(),
       device_desc.product_id()
     );
+
+    match usbmap.get(&(device_desc.vendor_id(), device_desc.product_id())) {
+      Some(m) => println!(
+        "found device {} at bus:{:03} dev:{:03}",
+        m,
+        device.bus_number(),
+        device.address()
+      ),
+      None => trace!(
+        "bus:{:03} dev:{:03} unknown",
+        device.bus_number(),
+        device.address()
+      ),
+    }
   }
+  Ok(())
 }
 
 pub async fn weather_report() {
@@ -51,8 +76,6 @@ pub async fn weather_report() {
     .await
     .expect("could not get forecast");
 
-  println!("---------------------------------------");
-  println!("--++-- WEATHER FORECAST --++--");
   for i in resf.properties.periods.iter() {
     println!("------------");
     println!("{:#?} -:- {:#?}", &i.name, &i.detailed_forecast);
@@ -65,6 +88,5 @@ pub fn print_org(path: &str) {
 
 pub async fn get_ip() {
   let ip = my_ip().await.expect("should return IP");
-  println!("---------------------------------------");
   println!("--++-- PUBLIC_IP === {:#?} --++--", ip);
 }
